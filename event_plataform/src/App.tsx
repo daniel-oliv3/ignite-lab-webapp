a3616bdb4d05a914175527f74cb6772a0818b748
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-06-22 08:52:33 -0300

    Up

--- event_plataform/src/App.tsx
+++ event_plataform/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <div>
-      <h1 className="text-2xl font-bold text-violet-500">Sapup3 na Área...</h1>
+      <h1 className="text-2xl font-bold text-violet-500">Sapup3 na Área</h1>
     </div>
   )
 }
