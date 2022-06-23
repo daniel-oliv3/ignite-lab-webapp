33986e02960c5cdc2b52cf5aac78692aa5b7d3e2
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-06-23 11:15:33 -0300

    Create Component Header

--- event_plataform/src/App.tsx
+++ event_plataform/src/App.tsx
@@ -1,33 +1,13 @@
-import { gql, useQuery } from '@apollo/client';
-import { useEffect } from 'react';
-import { client } from './lib/apollo';
 
-
-const GET_LESSONS_QUERY = gql `
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
-
-interface lesson {
-  id: string;
-  title: string;
-}
 
 function App() {
 
-const { data } = useQuery<{ lessons: lesson[] }>(GET_LESSONS_QUERY);
 
 
   return (
-    <ul>
-      {data?.lessons.map(lesson => {
-        return <li key={lesson.id}>{lesson.title}</li>
-      })}
-    </ul>
+    <div>
+     
+    </div>
   )
 }
 
---
+++ event_plataform/src/components/Header.tsx
@@ -0,0 +1,14 @@
+
+
+function Header() {
+
+
+
+    return (
+      <div>
+       
+      </div>
+    )
+  }
+  
+  export default Header
