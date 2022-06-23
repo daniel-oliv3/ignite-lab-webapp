83901a09b22d939255f75196a79beb7c8a158bf4
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-06-23 15:25:40 -0300

    Update tailwindcss

--- event_plataform/src/components/Lesson.tsx
+++ event_plataform/src/components/Lesson.tsx
@@ -5,7 +5,25 @@ function Lesson() {
 
     return (
       <a href="#">
-        <span>Terça • 22 de junho • 19h00</span>
+        <span className="text-gray-300">
+          Terça • 22 de junho • 19h00
+        </span>
+
+        <div className="rounded border border-gray-500 p-4 mt-2">
+          <header className="flex items-center justify-between">
+            <span className="text-sm text-blue-500 font-medium">
+              Conteúdo liberado
+            </span>
+            <span>
+              AO VIVO
+            </span>
+
+          </header>
+
+          <strong>
+            Abertura do evento Ignite labs
+          </strong>
+        </div>
       </a>
     )
 }
