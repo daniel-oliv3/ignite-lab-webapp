8eb5c891aa4fb44f5a4c682c0ebfee648f82c3a6
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-06-24 15:49:44 -0300

    Update Video.tsx

--- event_plataform/src/components/Video.tsx
+++ event_plataform/src/components/Video.tsx
@@ -1,3 +1,4 @@
+import { DiscordLogo, Lightning } from "phosphor-react"
 
 
 /* - */
@@ -20,7 +21,17 @@ function Video() {
               </p>
             </div>
 
-            <div className=""></div>            
+            <div className="flex flex-col gap-4">
+              <a href="#" className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center">
+                <DiscordLogo size={24}/>
+                Comunidade do Discord
+              </a>
+
+              <a href="#" className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center">
+                <Lightning size={24}/>
+                Acesse o desafio
+              </a>
+            </div>            
           </div>
           <div>
 
