45b27af97c68035333480a19ff608b24a70161e5
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-06-24 15:18:04 -0300

    Update Video.tsx

--- event_plataform/src/components/Video.tsx
+++ event_plataform/src/components/Video.tsx
@@ -5,7 +5,27 @@ function Video() {
 
     return (
       <div  className="flex-1">
-        Video
+        <div className="bg-black flex justify-center">
+          <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video"></div>   
+        </div>
+
+        <div className="p-8 max-w-[1100] mx-auto">
+          <div className="flex items-start gap-16">
+            <div className="flex-1">
+              <h1 className="text-2xl font-bold">
+                Aula 01 - Abertura do Ignite Lab
+              </h1>
+              <p className="mt-4 text-gray-200">
+                Nessa aula vamos dar início ao projeto criando a estrutura base da aplicação utilizando ReactJS, Vite e TailwindCSS. Vamos também realizar o setup do nosso projeto no GraphCMS criando as entidades da aplicação e integrando a API GraphQL gerada pela plataforma no nosso front-end utilizando Apollo Client.
+              </p>
+            </div>
+
+            <div className=""></div>            
+          </div>
+          <div>
+
+          </div>
+        </div>
       </div>
     )
   }
