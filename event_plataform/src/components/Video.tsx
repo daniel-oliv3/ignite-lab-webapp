2a5f8f8f61c4dc0fd39ac818819fb48c3e5189bb
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-06-28 11:41:10 -0300

    Update Video.tsx

--- event_plataform/src/components/Video.tsx
+++ event_plataform/src/components/Video.tsx
@@ -16,18 +16,22 @@ function Video() {
               <h1 className="text-2xl font-bold">
                 Aula 01 - Abertura do Ignite Lab
               </h1>
-              <p className="mt-4 text-gray-200">
+              <p className="mt-4 text-gray-200 leading-relaxed">
                 Nessa aula vamos dar início ao projeto criando a estrutura base da aplicação utilizando ReactJS, Vite e TailwindCSS. Vamos também realizar o setup do nosso projeto no GraphCMS criando as entidades da aplicação e integrando a API GraphQL gerada pela plataforma no nosso front-end utilizando Apollo Client.
               </p>
+
+              <div className="flex items-center gap-4 mt-6">
+                <img src="https://github.com/daniel-oliv3.png" alt="" />
+              </div>
             </div>
 
             <div className="flex flex-col gap-4">
-              <a href="#" className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700">
+              <a href="#" className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors">
                 <DiscordLogo size={24}/>
                 Comunidade do Discord
               </a>
 
-              <a href="#" className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center">
+              <a href="#" className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors">
                 <Lightning size={24}/>
                 Acesse o desafio
               </a>
